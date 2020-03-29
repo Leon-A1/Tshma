b5355c7819d9027a1e4896efec6fca4826710ce7
leon1107 <leonaviev@gmail.com>
2020-03-29 13:57:05 +0300

    bugs

--- app/models.py
+++ app/models.py
@@ -39,7 +39,7 @@ class User(UserMixin,db.Model):
         return User.query.get(user_id)
 
     def __repr__(self):
-        return '<User %r>' % (self.username)
+        return '%r' % (self.username)
 
 
 
